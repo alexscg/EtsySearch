89243c4942162e12a51a97a3cd9f724960938d33
Alex Shapiro <aleksey.shapiro@gmail.com>
2016-02-10 22:47:12 -0500

    Vanilla Javascript version

--- js/main.js
+++ js/main.js
@@ -1,49 +1,111 @@
-(function() {
-	$('#etsy-search').bind('submit', function() {
-		var api_key = "qmx7u3t1pymvbazv6jbdgrnf";
-		var terms = $('#etsy-terms').val();
-		var etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords="+terms+"&limit=12&includes=Images:1&api_key="+api_key;
-
-		$('#etsy-images').empty();
-		$('<p></p>').text('Searching for '+terms).appendTo('#etsy-images');
-
-		$.ajax({
-			url: etsyURL,
-			dataType: 'jsonp',
-			success: function(data) {
-				var newUrl = window.location.href.substr(0, window.location.href.indexOf('?')) + "?st=" + terms;
-				window.history.pushState({"html":data, "pageTitle":'etsysearch' + terms}, "", newUrl);
+function getData(data) {
+	if (data.ok) {
+		var oImages = document.getElementById('etsy-images');
+		var oTerms = document.getElementById('etsy-terms');
+		
+		updateStatus('Results for ', oTerms.value);
+		
+		if (data.count > 0) {
+			var items = data.results;
+			var row = document.createElement('div');
+			row.className = 'row';
+			
+			for (var i = 0, l = items.length; i<l; i++) {
+				var img = document.createElement('img'),
+					divTitle = document.createElement('div'),
+					divImgWrap = document.createElement('div'),
+					divPrice = document.createElement('div'),
+					divDesc = document.createElement('div'),
+					anchor = document.createElement('a');
 				
-				if (data.ok) {
-					$('#etsy-images').empty();
-					if (data.count > 0) {
-						$.each(data.results, function(i,item) {
-							$("<img/>").attr({"src": item.Images[0].url_75x75, "class": "panel"}).appendTo("#etsy-images").wrap(
-								"<div class='col-md-3'><a class='well' href='" + item.url + "'></a></div>"
-							);
-							$("#etsy-images > .col-md-3 img:last").before('<div class="panel title">' + item.title + '</div>');
-							$("#etsy-images > .col-md-3 img:last").before('<div class="panel price">' + item.currency_code + ' ' + item.price + '</div>');
-							$("#etsy-images > .col-md-3 img:last").after('<div class="panel desc">' + item.description + '</div>');
-							if (i%4 == 3) {
-								$("#etsy-images").append('<div class="row"></div>');
-								$("#etsy-images > .col-md-3").appendTo('#etsy-images .row:last');
-							}
-						});
-					} else {
-						$('<p>No results.</p>').appendTo('#etsy-images');
-					}
-				} else {
-					$('#etsy-images').empty().append('<p>Data error.</p>');
-					alert(data.error);
+				divTitle.innerHTML = items[i].title;
+				divTitle.className = 'panel title';
+				divPrice.innerHTML = items[i].currency_code + ' ' + items[i].price;
+				divPrice.className = 'panel price';
+				divDesc.innerHTML = items[i].description;
+				divDesc.className = 'panel desc';
+				img.src = items[i].Images[0].url_75x75;
+				img.className = 'panel';
+				divImgWrap.className = 'col-md-3';
+				anchor.className = 'well';
+				anchor.href = items[i].url;
+				
+				anchor.appendChild(divTitle);
+				anchor.appendChild(divPrice);
+				anchor.appendChild(img);
+				anchor.appendChild(divDesc);
+				divImgWrap.appendChild(anchor);
+				row.appendChild(divImgWrap);
+				
+				if (i%4 == 3) {
+					oImages.appendChild(row);
+					row = document.createElement('div');
+					row.className = 'row';
 				}
 			}
-		});
+			
+			var newUrl = window.location.href.substr(0, window.location.href.indexOf('?')) + '?st=' + oTerms.value;
+			window.history.pushState({'html':data, 'pageTitle':'etsysearch' + oTerms.value}, '', newUrl);
+		}
+	}
+	else {
+		updateStatus('Communication error: ', error);
+	}
+}
 
-		return false;
+function updateStatus(){
+	var textNode = document.createTextNode(arguments[0]);
+	var domNode = document.getElementById('etsy-status');
+	domNode.innerHTML = '';
+	domNode.appendChild(textNode);
+
+	if (arguments.length > 1) {
+		var span = document.createElement('span');
+		span.innerHTML = arguments[1];
+		domNode.appendChild(span);
+	}
+}
+		
+document.addEventListener('DOMContentLoaded', function(event) {
+	document.getElementById('etsy-search-button').addEventListener('click', function() {
+		submitForm();
 	});
 	
-	if(window.location.search.length){
-		$('#etsy-terms').val(unescape(window.location.search.split('=')[1]));
-		$('#etsy-search').submit();
+	function stopRKey(evt) { 
+		evt = (evt) ? evt : ((event) ? event : null); 
+		var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); 
+		if ((evt.keyCode == 13) && (node.type === 'text')) {
+			submitForm();
+			return false;
+		}
+	} 
+	document.onkeypress = stopRKey; 
+
+	var oTerms = document.getElementById('etsy-terms');
+	var oImages = document.getElementById('etsy-images');
+	
+	function submitForm() {
+		var api_key = 'qmx7u3t1pymvbazv6jbdgrnf';
+		var etsyURL = 'https://openapi.etsy.com/v2/listings/active.js?callback=getData&keywords=' + oTerms.value + '&limit=12&includes=Images:1&api_key=' + api_key;
+	
+		oImages.innerHTML = '';
+
+		if (oTerms.value.length) {
+			updateStatus('Searching for ', oTerms.value);
+
+			var script = document.createElement('script');
+			script.src = etsyURL;
+			document.getElementsByTagName('head')[0].appendChild(script);
+		}
+		else {
+			updateStatus('Please enter search terms');
+		}
+		
+		return false;
 	}
-})();
+
+	if(window.location.search.length){
+		oTerms.value = unescape(window.location.search.split('=')[1]);
+		submitForm();
+	}
+});
